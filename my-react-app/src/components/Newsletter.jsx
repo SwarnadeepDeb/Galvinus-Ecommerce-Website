13121e71ece7deb1fe6add3a6ea47dc99ce3255c
SwarnadeepDeb <swarnadeepdebdevelopment@gmail.com>
2024-12-18 16:49:51 +0530

    Added newsletter section

--- my-react-app/src/components/Newsletter.jsx
+++ my-react-app/src/components/Newsletter.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import './Newsletter.css'; // Import your CSS file for styling
-
+import React from "react";
+import "./Newsletter.css"; // Import your CSS file for styling
 
 // https://woodmart.b-cdn.net/wp-content/uploads/2015/12/newsletter-new.jpg?id=9309
 const Newsletter = () => {
   return (
     <div className="newsletter-container">
-      <h4 className="newsletter-title">JOIN OUR NEWSLETTER NOW</h4>
-      <div className="newsletter-form">
-        <input
-          type="email"
-          placeholder="Your email address"
-          className="email-input"
-        />
-        <button className="signup-btn">Sign Up</button>
+      <div className="main-newsletter-container">
+        <h4 className="newsletter-title">JOIN OUR NEWSLETTER NOW</h4>
+        <div className="newsletter-form">
+          <input
+            type="email"
+            placeholder="Your email address"
+            className="email-input"
+          />
+          <button className="signup-btn">Sign Up</button>
+        </div>
+        <p className="privacy-policy">
+          Will be used in accordance with our <span>Privacy Policy</span> 
+        </p>
       </div>
-      <p className="privacy-policy">
-        Will be used in accordance with our Privacy Policy
-      </p>
     </div>
   );
 };
